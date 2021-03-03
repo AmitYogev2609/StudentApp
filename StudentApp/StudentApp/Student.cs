cf0a253ff413c583f7694c2d8a4bdcec11af7b21
AmitYogev2609 <amity2609@gmail.com>
2021-03-03 13:22:48 +0200

    gfhgfgf

--- StudentApp/StudentApp/Student.cs
+++ StudentApp/StudentApp/Student.cs
@@ -1,18 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.ComponentModel;
 namespace StudentApp
 {
-   public class Student
-    { 
-            public string Name { get; set; }
-            public string Address { get; set; }
-            public string Class { get; set; }
-            public string ClassNumber { get; set; }
-            public DateTime BirthDate { get; set; }
-            public bool LikeMath { get; set; }
-            public bool LikeCS { get; set; }
+   public class Student : INotifyPropertyChanged
+    {
+        private string name;
+        private string address;
+        private string clas;
+        private string classnum;
+        private DateTime date;
+        private bool cs;
+        private bool math;
+            public string Name { get { return name; }
+            set {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+            public string Address {
+            get { return address; }
+            set
+            {
+                address = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Address"));
+            }
+        }
+            public string Class
+        {
+            get { return clas; }
+            set
+            {
+                clas = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Class"));
+            }
+        }
+            public string ClassNumber {
+            get { return classnum; }
+            set
+            {
+                classnum = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ClassNumber"));
+            }
+        }
+            public DateTime BirthDate {
+            get { return date; }
+            set
+            {
+                date = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BirthDate"));
+            }
+        }
+            public bool LikeMath {
+            get { return math; }
+            set
+            {
+               math = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LikeMath"));
+            }
+        }
+            public bool LikeCS {
+            get { return cs; }
+            set
+            {
+                cs = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LikeCS"));
+            }
+        }
             public Student()
             {
                 LikeMath = false;
@@ -24,6 +79,7 @@ namespace StudentApp
             
 
             }
-        
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
